0ebd05ae50b4b5be028520893573b73a13798c49
Sebastian Fröstl <sebastian@froestl.com>
2014-11-11 17:36:17 +0100

    add manual bootstrapping & angular 2 slides

--- examples.js
+++ examples.js
@@ -1,3 +1,11 @@
 (function (sandbox) {
-    sandbox.angular.module('exampleApp', ['ngMessages']);
+    sandbox.angular.module('example1', []);
+    sandbox.angular.module('example2', ['ngMessages']);
+
+    var examples = document.querySelectorAll('[ng-example]');
+
+    Array.prototype.forEach.call(examples, function (element) {
+        sandbox.angular.bootstrap(element, [element.attributes['ng-example'].value]);
+    });
+
 })(this);
